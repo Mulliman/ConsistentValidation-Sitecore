46705386a83f46a467221f0e4eab9e8ccf1c345c
Sam Mullins <sammullins.co.uk@gmail.com>
2016-04-17 17:53:08 +0100

    Added a couple of Sitecore message providers.

---
+++ Src/ConsistentValidation.Sc/ConsistentValidation.Sc/Messages/SitecoreDescendantMessageProvider.cs
@@ -0,0 +1,31 @@
+﻿using ConsistentValidation.Messages;
+using ConsistentValidation.Rules;
+using Sitecore.Configuration;
+using Sitecore.Data;
+
+namespace ConsistentValidation.Sc.Messages
+{
+    public class SitecoreDescendantMessageProvider : IMessageProvider
+    {
+        private readonly Database _db;
+        private readonly string _messagesRootPath;
+        private readonly string _messageFieldName;
+
+        public SitecoreDescendantMessageProvider(string messagesRootPath, string database, string messageFieldName)
+        {
+            _messagesRootPath = messagesRootPath;
+            _messageFieldName = messageFieldName;
+
+            _db = Factory.GetDatabase(database);
+        }
+
+        public string GetMessageFor(IValidationRuleData ruleData)
+        {
+            var query = _messagesRootPath.TrimEnd('/') + "//*[@@Name = '" + ruleData.MessageId+ "']";
+
+            var item = _db.SelectSingleItem(query);
+
+            return item[_messageFieldName];
+        }
+    }
+}
---
+++ Src/ConsistentValidation.Sc/ConsistentValidation.Sc/Messages/SitecoreFieldsOnItemMessageProvider.cs
@@ -0,0 +1,29 @@
+﻿using ConsistentValidation.Messages;
+using ConsistentValidation.Rules;
+using Sitecore.Configuration;
+using Sitecore.Data;
+
+namespace ConsistentValidation.Sc.Messages
+{
+    public class SitecoreFieldsOnItemMessageProvider : IMessageProvider
+    {
+        private readonly Database _db;
+        private readonly string _messagesPath;
+
+        public SitecoreFieldsOnItemMessageProvider(string messagesPath, string database)
+        {
+            _messagesPath = messagesPath;
+
+            _db = Factory.GetDatabase(database);
+        }
+
+        public string GetMessageFor(IValidationRuleData ruleData)
+        {
+            var query = _messagesPath;
+
+            var item = _db.SelectSingleItem(query);
+
+            return item[ruleData.MessageId];
+        }
+    }
+}
