271242c35fd6f2bc7a0185953a065c9abaa8ccf2
Sam Mullins <sammullins.co.uk@gmail.com>
2016-04-17 18:00:06 +0100

    Missing cache clearing pipeline class.

---
+++ Src/ConsistentValidation.Sc/ConsistentValidation.Sc/Pipelines/Publish/ClearMessageCacheOnPublish.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Publishing.Pipelines.Publish;
+
+namespace ConsistentValidation.Sc.Pipelines.Publish
+{
+    /// <summary>
+    /// This clears the message cache when a Sitecore item is published.
+    /// </summary>
+    /// <remarks>Add <processor type="ConsistentValidation.Sc.Pipelines.Publish.ClearMessageCacheOnPublish,ConsistentValidation.Sc" /> to the 'publish' pipeline.</remarks>
+    public class ClearMessageCacheOnPublish : PublishProcessor
+    {
+        public override void Process(PublishContext context)
+        {
+            Configuration.MessageCache.ClearCache();
+        }
+    }
+}
